086f03d058f227f668969e24c75296dc8986d3ee
dsal0506 <115323449+dsal0506@users.noreply.github.com>
2023-11-29 13:28:29 -0600

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    vueIndentScriptAndStyle: ['error', '2'],
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+}
